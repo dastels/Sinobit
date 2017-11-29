643d76cc83c9bdfb8ec6fe83fc5d13a8be4b7efb
Dave Astels <dastels@daveastels.com>
2017-11-29 12:13:34 -0500

    Add connector IO pin definitions

--- src/sinobit.h
+++ src/sinobit.h
@@ -12,6 +12,28 @@
 #define SINOBIT_PAD_P4 (4)
 #define SINOBIT_PAD_P5 (10)
 
+#define SINOBIT_P0 (0)
+#define SINOBIT_P1 (1)
+#define SINOBIT_P2 (2)
+#define SINOBIT_P3 (3)
+#define SINOBIT_P4 (4)
+#define SINOBIT_P5 (5)
+#define SINOBIT_P6 (6)
+#define SINOBIT_P7 (7)
+#define SINOBIT_P8 (8)
+#define SINOBIT_P9 (9)
+#define SINOBIT_P10 (10)
+#define SINOBIT_P11 (11)
+#define SINOBIT_P12 (12)
+#define SINOBIT_P13 (13)
+#define SINOBIT_P14 (14)
+#define SINOBIT_P15 (15)
+#define SINOBIT_P16 (16)
+/* #define SINOBIT_P17 (17) */
+/* #define SINOBIT_P18 (18) */
+/* #define SINOBIT_P19 (19) */
+#define SINOBIT_P20 (20)
+
 #define SINOBIT_BUTTON_A (5)
 #define SINOBIT_BUTTON_B (11)
 
